9649ceb4b40890ae8bcbada2b28f987c2273c22d
Matthew Hyrka <matthew.hyrka@avalara.com>
2019-11-04 07:23:03 -0700

    Removed dead code

--- resources/views/readinglist.blade.php
+++ resources/views/readinglist.blade.php
@@ -2,13 +2,6 @@
 @section('mybooks')
 <div class="alert alert-danger">
 
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
-    <h1>piss ass</h1>
 
 </div>
 @endsection
